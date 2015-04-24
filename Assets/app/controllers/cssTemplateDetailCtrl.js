9a573ff39d1f64ba763315a66849458dcc4db810
Victor Antofica <victorantos@gmail.com>
2015-04-24 18:27:32 +0100

    added iframe for template preview

--- Assets/app/controllers/cssTemplateDetailCtrl.js
+++ Assets/app/controllers/cssTemplateDetailCtrl.js
@@ -1,18 +1,19 @@
 ﻿angular.module('cssTemplateDetail', [])
-    .controller('cssTemplateDetailCtrl', ['$rootScope', '$scope', '$routeParams', '$http', 'csstemplates', 'filterFilter', function ($rootScope, $scope, $routeParams, $http, csstemplates, filterFilter) {
+    .controller('cssTemplateDetailCtrl', ['$rootScope', '$scope', '$routeParams', '$http', 'csstemplates', 'filterFilter', '$sce', function ($rootScope, $scope, $routeParams, $http, csstemplates, filterFilter, $sce) {
         $scope.cssTemplateName = $routeParams.cssTemplateId;
         
         $scope.cssTemplate = filterFilter(csstemplates.getLoadedTemplates(), $scope.cssTemplateName);
 
-        if ($scope.cssTemplate == null)
-        {
+        if ($scope.cssTemplate == null) {
             csstemplates.byTemplateName($scope.cssTemplateName).then(function (resp) {
 
                 if (resp.data.list != null && resp.data.list.length) {
                     $scope.cssTemplate = resp.data.list[0];
                     console.dir($scope.cssTemplate);
+                    $scope.templateUrl = $scope.cssTemplate.previewUrl;
                 }
             });
         }
-       
+        else
+            $scope.templateUrl = $scope.cssTemplate.previewUrl;
     }]);
